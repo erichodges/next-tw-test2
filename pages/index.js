607251889d23468190637f7e76bd4ac14790b69d
Eric Hodges <ehodges2004@gmail.com>
2021-07-30 16:02:08 -0700

    Add: Daisy UI

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       priority="true"
     />
     <h1
-      className="absolute transform -translate-x-1/2 -translate-y-20 md:-translate-y-1/3 sm:-translate-y-1/3 xxs:-translate-y-1/2 xl:text-9xl lg:text-8xl base:text-7xl sm:text-6xl xs:text-5xl xxs:text-4xl top-1/2 left-1/2"
+      className="absolute transform -translate-x-1/2 -translate-y-20 md:-translate-y-1/3 sm:-translate-y-1/3 xxs:-translate-y-1/2 xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl top-1/2 left-1/2"
     >
       Test Text
     </h1>
