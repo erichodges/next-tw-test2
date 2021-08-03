1781559b2101002ad120782ef5c23e333e0ea8d6
Eric Hodges <ehodges2004@gmail.com>
2021-08-02 19:27:58 -0700

    Chore: Added page2 to test DaisyUI

---
+++ pages/page2.js
@@ -0,0 +1,21 @@
+// import Image from "next/image";
+
+export default function Page2() {
+  return (
+    <div className="relative content-center w-screen">
+    <Image
+      src="https://res.cloudinary.com/dn3baky3u/image/upload/v1580246844/gatsby-cloudinary/evora-gt-side.jpg"
+      alt="car"
+      width="2400"
+      height="1200"
+      layout="responsive"
+      priority="true"
+    />
+    <h1
+      className="absolute transform -translate-x-1/2 -translate-y-20 md:-translate-y-1/3 sm:-translate-y-1/3 xxs:-translate-y-1/2 xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl top-1/2 left-1/2"
+    >
+      Test Text
+    </h1>
+  </div>
+  )
+}
